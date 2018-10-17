52d62b17d866c5caf560f647b123914f60dee91c
mattmccreesh <mmccrees@stevens.edu>
2018-10-17 17:21:58 -0400

    added FIPS

--- LocData.py
+++ LocData.py
@@ -1,12 +1,24 @@
 import geocoder
+import requests
+import urllib
 
 class LocData:
     
     def __init__(self, ipAddrs):
         self.ipAddrs = ipAddrs
         self.states = []
+        self.coords = []
+        self.FIPS = []
         for address in ipAddrs:
             g = geocoder.ip(address)
             self.states.append(g.state)
+            self.coords.append(g.latlng)
+            self.FIPS.append(self.getFIPS(g.lat, g.lng))
 
-        
+    def getFIPS(self, lat, lon):
+        params = urllib.urlencode({'latitude': lat, 'longitude':lon, 'format':'json'})
+        url = 'https://geo.fcc.gov/api/census/block/find?' + params
+        response = requests.get(url)
+        data = response.json()
+        return data['County']['FIPS']
+
