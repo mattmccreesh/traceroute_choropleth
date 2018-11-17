5958d03386933fd1b4a3e49bc578f4cea4700fc8
Min Su Park <mpark3@stevens.edu>
2018-11-16 19:21:58 -0500

    Updated so that the FipsDict includes every county

--- Choropleth.py
+++ Choropleth.py
@@ -1,6 +1,7 @@
 import plotly.plotly as py
 import plotly.tools as plottools
 import plotly.figure_factory as ff
+import plotly.io as pio
 
 import numpy as np
 import pandas as pd
@@ -19,25 +20,37 @@ class Choropleth:
 		fips = data.keys()
 		values = data.values()
 		intervals = range(0,51,5)
-		colors = ['#FAFF70','#FAEB69','#FAD862','#FBC55B','#FBB154','#FC9E4D','#FC8B46','#FD783F','#FD6438','#FE5131','#FE3E2A','#FF2B24']
+
+		colors = ["#ffffe0", "#ffffe0", "#ffd196", "#ffb67f", "#ff9b6f", "#f98065", "#ee665d", "#e14d54", "#cf3548", "#bb1e37", "#a40921", "#8b0000"]
+		# colors = ["#FFFED1","#FFEAB6","#FFD5A6","#FFC096","#FFAB85","#FF9675","#FF8265","#FF6D55","#FF5844","#FF4334","#FF2E24","#FF1A14"]
+		# colors = ['#FAFF70','#FAEB69','#FAD862','#FBC55B','#FBB154','#FC9E4D','#FC8B46','#FD783F','#FD6438','#FE5131','#FE3E2A','#FF2B24']
 
 		fig = ff.create_choropleth(
-		    fips=fips, values=values, scope=['USA'],
-		    show_state_data=True,
-		    colorscale=colors,
-		    binning_endpoints=intervals,
-		    show_hover=True, centroid_marker={'opacity': 1},
-		    asp=2.9, title='West Coast Sites Traceroute',
-		    legend_title='Number of Hits',
-		    round_legend_values=True
+			fips=fips, values=values, scope=['USA'],
+			show_state_data=True,
+			colorscale=colors,
+			binning_endpoints=intervals,
+			show_hover=True, centroid_marker={'size':6,'opacity': 1, 'color':'black'},
+			asp=2.9, title='West Coast Sites Traceroute',
+			legend_title='Number of Hits',
+			round_legend_values=True
 		)
-		py.plot(fig, filename='Traceroute Choropleth')
+		# py.plot(fig, filename='Traceroute Choropleth')
+		pio.write_image(fig, "Output_Heatmap.png")
+		print("Heatmap Created!")
 
 	def createPlot(self):
 		self.plotData(self.locData)
 
 	def testPlot(self):
 		testData = {'34017':1,'20155':6,'36059':12,'53033':17,'36001':21,'32510':27,'33001':34,'40001':39,'04001':40,'33001':45,'39001':57}
+		counties_text_file = open("national_county.txt","r")
+		for line in counties_text_file:
+			line = line.split(',')
+			FIPS = line[1]+line[2]
+			if FIPS not in testData.keys():
+				testData[FIPS] = 1
+		counties_text_file.close()
 		self.plotData(testData)
 
 	@staticmethod
--- LocData.py
+++ LocData.py
@@ -5,67 +5,70 @@ from urllib import urlencode
 #to provide similar objects to geocoder
 #allows us to have more modularity
 class GeoData(object):
-    def __init__(self, country, state, lat, lng):
-        self.country = country
-        self.state = state
-        self.lat = lat
-        self.lng = lng
+	def __init__(self, country, state, lat, lng):
+		self.country = country
+		self.state = state
+		self.lat = lat
+		self.lng = lng
 
 class LocData:
-    
-    def __init__(self):
-        self.ipAddrs = []
-        self.coords = []
-        self.states = []
-        self.countries = []
-        self.FIPS = []
-        self.FipsDict = {}
+	
+	def __init__(self):
+		self.ipAddrs = []
+		self.coords = []
+		self.states = []
+		self.countries = []
+		self.FipsList = []
+		self.FipsDict = {}
+		self.ipResponse = {}
+		counties_text_file = open("national_county.txt","r")
+		for line in counties_text_file:
+			line = line.split(',')
+			FIPS = line[1]+line[2]
+			self.FipsDict[FIPS] = 0
+		counties_text_file.close()
 
-    def getFIPSbyLatLong(self, lat, lon):
-        params = urlencode({'latitude': lat, 'longitude':lon, 'format':'json'})
-        url = 'https://geo.fcc.gov/api/census/block/find?' + params
-        response = get(url)
-        # print(str(lat) + "," + str(lon))
-        if response.text.find("<html>") == -1:
-            print(response)
-            data = response.json()['County']['FIPS']
-            if data is not None:
-                return data.encode('ascii','ignore')
-            else:
-                print("FCC FIPS is None")
-        else:
-            print("Bad Response from FCC")
+	def getFIPSbyLatLong(self, address,lat, lon):
+		params = urlencode({'latitude': lat, 'longitude':lon, 'format':'json'})
+		url = 'https://geo.fcc.gov/api/census/block/find?' + params
+		response = get(url)
+		# print(str(lat) + "," + str(lon))
+		if response.text.find("<html>") == -1:
+			# print(response)
+			self.ipResponse[address] = response
+			data = response.json()['County']['FIPS']
+			if data is not None:
+				return data.encode('ascii','ignore')
+			else:
+				print("FCC FIPS is None")
+		else:
+			print("Bad Response from FCC")
 
-    #replaces geocoder module with GET reqeust to API to avoid query limit
-    def getGeoDataFromIP(self, testIP):
-        sleep(0.25)
-        url = 'http://api.petabyet.com/geoip/' + testIP
-        response = get(url)
-        jsonData = response.json()
-        # print(jsonData)
-        lat = jsonData['latitude']
-        lng = jsonData['longitude']
-        country = jsonData['country']
-        if 'region' in jsonData.keys():
-            state = jsonData['region']
-        else:
-            state = ""
-        return GeoData(country, state, lat, lng)
+	#replaces geocoder module with GET reqeust to API to avoid query limit
+	def getGeoDataFromIP(self, testIP):
+		# sleep(0.25)
+		url = 'http://api.petabyet.com/geoip/' + testIP
+		response = get(url)
+		jsonData = response.json()
+		# print(jsonData)
+		lat = jsonData['latitude']
+		lng = jsonData['longitude']
+		country = jsonData['country']
+		if 'region' in jsonData.keys():
+			state = jsonData['region']
+		else:
+			state = ""
+		return GeoData(country, state, lat, lng)
 
-    def genFIPSList(self, ipAddrs):
-        self.ipAddrs.extend(ipAddrs)
-        for address in ipAddrs:
-            g = self.getGeoDataFromIP(address)
-            self.countries.append(g.country)
-            self.states.append(g.state)
-            self.coords.append([g.lat,g.lng])
-            fip = self.getFIPSbyLatLong(g.lat, g.lng)
-            if fip is not None:
-                self.FIPS.append(fip)
-                if fip not in self.FipsDict:
-                    self.FipsDict[fip] = 1
-                else:
-                    self.FipsDict[fip] = self.FipsDict[fip] + 1
-            else:
-                print(g.country)
+	def genFIPSList(self, ipAddrs):
+		self.ipAddrs.extend(ipAddrs)
+		for address in ipAddrs:
+			g = self.getGeoDataFromIP(address)
+			self.countries.append(g.country)
+			self.states.append(g.state)
+			self.coords.append([g.lat,g.lng])
+			fip = self.getFIPSbyLatLong(address,g.lat, g.lng)
+			if fip is not None:
+				self.FipsList.append(fip)
+				self.FipsDict[fip] += 1
 
